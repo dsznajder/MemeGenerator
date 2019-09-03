eac1bbcee383929e2c1e5e91b0d4414561382a0e
Damian Sznajder <damsznajder@gmail.com>
2019-09-03 11:30:40 +0200

    First listing

--- src/scenes/Main/index.js
+++ src/scenes/Main/index.js
@@ -1,70 +1,44 @@
 // @flow
 
-import React, { Component } from 'react'
-import { ScrollView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
 
-import Button from 'src/components/Button'
-import Input from 'src/components/Input'
 import { primary } from 'src/styles/colors'
 
-type State = {
-  error: string,
-  first: string,
-  second: string,
+const data: Array<number> = Array(100)
+  .fill()
+  .map((element: null, index: number): number => index)
+
+const App = () => {
+  const renderItem = ({ item }: { item: number }) => (
+    <View style={styles.item}>
+      <Text style={styles.text}>{item}</Text>
+    </View>
+  )
+
+  return (
+    <FlatList
+      contentContainerStyle={styles.list}
+      data={data}
+      keyExtractor={item => `${item}`}
+      numColumns={20}
+      renderItem={renderItem}
+    />
+  )
 }
 
-export default class App extends Component<{}, State> {
-  state = {
-    error: '',
-    first: '',
-    second: '',
-  }
-
-  _handleButtonPress = () => {
-    this.setState(prevState => ({ error: prevState.error ? '' : 'This is errorMessage' }))
-  }
-
-  _handleChangeText = key => value => this.setState({ [key]: value })
-
-  render() {
-    const { error, first, second } = this.state
-
-    return (
-      <View>
-        <ScrollView contentContainerStyle={styles.container}>
-          <Input label="Text" onChangeText={this._handleChangeText('first')} value={first} />
-          <Input
-            error={error}
-            label="Text with possible error"
-            onChangeText={this._handleChangeText('second')}
-            value={second}
-          />
-
-          <Button color={primary} onPress={this._handleButtonPress} style={styles.button}>
-            <Text>{'Make Error'}</Text>
-          </Button>
-
-          <Text style={styles.placeholder}>{'Test'}</Text>
-          <Text style={styles.placeholder}>{'Test'}</Text>
-          <Text style={styles.placeholder}>{'Test'}</Text>
-          <Text style={styles.placeholder}>{'Test'}</Text>
-        </ScrollView>
-      </View>
-    )
-  }
-}
+export default App
 
 const styles = StyleSheet.create({
-  button: {
-    backgroundColor: primary,
-    borderRadius: 30,
-    padding: 15,
-  },
-  container: {
+  item: {
     alignItems: 'center',
+    flex: 1,
     justifyContent: 'center',
   },
-  placeholder: {
-    height: 100,
+  list: {
+    paddingTop: 20,
+  },
+  text: {
+    color: primary,
   },
 })
