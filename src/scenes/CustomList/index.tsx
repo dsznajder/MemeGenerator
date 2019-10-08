543ffc203fe38b147951319853b892033bfab6cd
Damian Sznajder <damsznajder@gmail.com>
2019-10-08 22:13:49 +0200

    Working image picker

--- src/scenes/CustomList/index.tsx
+++ src/scenes/CustomList/index.tsx
@@ -1,12 +1,39 @@
+import ImagePicker from 'react-native-image-picker';
 import React from 'react';
 import { Button, View } from 'react-native';
 
 import { secondary } from '~/styles/colors';
 
+const imagePickerOptions = {
+  title: 'Select Meme',
+  storageOptions: {
+    skipBackup: true,
+    path: 'images',
+  },
+};
+
 const CustomList = () => {
+  const openImagePicker = () => {
+    ImagePicker.showImagePicker(imagePickerOptions, response => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      } else {
+        // const source = { uri: response.uri };
+        // You can also display the image using data:
+        // const source = { uri: 'data:image/jpeg;base64,' + response.data };
+      }
+    });
+  };
+
   return (
     <View>
-      <Button title="upload" color={secondary} onPress={() => {}} />
+      <Button title="upload" color={secondary} onPress={openImagePicker} />
     </View>
   );
 };
